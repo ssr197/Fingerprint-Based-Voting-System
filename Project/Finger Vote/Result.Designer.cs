ecbcc5a475cff858f97ad075a1715db4b8b7d67d
Saurabh Kumar <saurabh.kumar1@rxlogix.com>
2019-11-28 17:26:56 +0530

    Added result

---
+++ Project/Finger Vote/Result.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace Finger_ATM
+{
+    partial class Result
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Navy;
+            this.label1.Location = new System.Drawing.Point(35, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(264, 37);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Calculate Result";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Finger_ATM.Properties.Resources.Line;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 75);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1275, 18);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.comboBox5);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Location = new System.Drawing.Point(477, 99);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(417, 237);
+            this.groupBox2.TabIndex = 39;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Election";
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Location = new System.Drawing.Point(136, 36);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(254, 28);
+            this.comboBox5.TabIndex = 39;
+            this.comboBox5.SelectedIndexChanged += new System.EventHandler(this.comboBox5_SelectedIndexChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(136, 87);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(254, 131);
+            this.textBox3.TabIndex = 38;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(26, 39);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(104, 20);
+            this.label6.TabIndex = 36;
+            this.label6.Text = "Election ID  :-";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(66, 90);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 20);
+            this.label5.TabIndex = 37;
+            this.label5.Text = "Topic  :-";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Enabled = false;
+            this.groupBox1.Location = new System.Drawing.Point(196, 348);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(417, 282);
+            this.groupBox1.TabIndex = 38;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Candidates";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Candidate 1  :-";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(146, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(242, 26);
+            this.textBox1.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Candidate 2  :-";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(146, 98);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(242, 26);
+            this.textBox2.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Candidate 3  :-";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(146, 159);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(242, 26);
+            this.textBox4.TabIndex = 9;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(28, 223);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(112, 20);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Candidate 4  :-";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(146, 220);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(242, 26);
+            this.textBox5.TabIndex = 9;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBox8);
+            this.groupBox3.Controls.Add(this.label10);
+            this.groupBox3.Controls.Add(this.textBox9);
+            this.groupBox3.Controls.Add(this.label11);
+            this.groupBox3.Enabled = false;
+            this.groupBox3.Location = new System.Drawing.Point(758, 348);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(417, 282);
+            this.groupBox3.TabIndex = 38;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Results";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(117, 162);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.Size = new System.Drawing.Size(265, 26);
+            this.textBox8.TabIndex = 9;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(43, 165);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(68, 20);
+            this.label10.TabIndex = 8;
+            this.label10.Text = "Votes  :-";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(117, 79);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.ReadOnly = true;
+            this.textBox9.Size = new System.Drawing.Size(265, 26);
+            this.textBox9.TabIndex = 9;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(35, 82);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(76, 20);
+            this.label11.TabIndex = 8;
+            this.label11.Text = "Winner  :-";
+            // 
+            // Result
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1370, 750);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Result";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Result";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Result_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label11;
+    }
+}
---
+++ Project/Finger Vote/Result.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Finger_ATM
+{
+    public partial class Result : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=ADMIN-PC;Initial Catalog=FingerVote;Integrated Security=True");
+        
+        public Result()
+        {
+            InitializeComponent();
+        }
+
+        private void Result_Load(object sender, EventArgs e)
+        {
+            string date = DateTime.Now.ToString("yyyy/MM/dd");
+            SqlDataAdapter da = new SqlDataAdapter("Select EId from Election where EndDate < '" + date + "' And Result ='N/A'", con);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+
+            int count = ds.Tables[0].Rows.Count;
+            for (int i = 0; i < count; i++)
+            {
+                comboBox5.Items.Add(ds.Tables[0].Rows[i][0].ToString());
+            }
+        
+        }
+
+        private void comboBox5_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("Select * from Election where EId = '" + comboBox5.Text + "'", con);
+            con.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            dr.Read();
+            textBox3.Text = dr[1].ToString();
+            string cou = dr[2].ToString();
+            int count = Convert.ToInt32(cou);
+
+            string[] names = new string[count];
+            int[] vote = new int[count];
+
+            if (cou == "2")
+            {
+                textBox1.Text = dr[3].ToString();
+                textBox2.Text = dr[4].ToString();
+                textBox4.Text = "N/A";
+                textBox5.Text = "N/A";
+                names[0] = textBox1.Text;
+                names[1] = textBox2.Text;
+                con.Close();
+
+                SqlDataAdapter da = new SqlDataAdapter("Select * from Vote where EID = '"+comboBox5.Text+"' And EC1 ='1' ", con);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                vote[0] = ds.Tables[0].Rows.Count;
+
+                da = new SqlDataAdapter("Select * from Vote where EID = '" + comboBox5.Text + "' And EC2 ='1' ", con);
+                ds = new DataSet();
+                da.Fill(ds);
+                vote[1] = ds.Tables[0].Rows.Count;
+            }
+            else if (cou == "3")
+            {
+                textBox1.Text = dr[3].ToString();
+                textBox2.Text = dr[4].ToString();
+                textBox4.Text = dr[5].ToString();
+                textBox5.Text = "N/A";
+                names[0] = textBox1.Text;
+                names[1] = textBox2.Text;
+                names[2] = textBox4.Text;
+                con.Close();
+
+                SqlDataAdapter da = new SqlDataAdapter("Select * from Vote where EID = '" + comboBox5.Text + "' And EC1 ='1' ", con);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                vote[0] = ds.Tables[0].Rows.Count;
+
+                da = new SqlDataAdapter("Select * from Vote where EID = '" + comboBox5.Text + "' And EC2 ='1' ", con);
+                ds = new DataSet();
+                da.Fill(ds);
+                vote[1] = ds.Tables[0].Rows.Count;
+
+                da = new SqlDataAdapter("Select * from Vote where EID = '" + comboBox5.Text + "' And EC3 ='1' ", con);
+                ds = new DataSet();
+                da.Fill(ds);
+                vote[2] = ds.Tables[0].Rows.Count;
+            }
+            else if (cou == "4")
+            {
+                textBox1.Text = dr[3].ToString();
+                textBox2.Text = dr[4].ToString();
+                textBox4.Text = dr[5].ToString();
+                textBox5.Text = dr[6].ToString();
+                names[0] = textBox1.Text;
+                names[1] = textBox2.Text;
+                names[2] = textBox4.Text;
+                names[3] = textBox5.Text;
+                con.Close();
+                SqlDataAdapter da = new SqlDataAdapter("Select * from Vote where EID = '" + comboBox5.Text + "' And EC1 ='1' ", con);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                vote[0] = ds.Tables[0].Rows.Count;
+
+                da = new SqlDataAdapter("Select * from Vote where EID = '" + comboBox5.Text + "' And EC2 ='1' ", con);
+                ds = new DataSet();
+                da.Fill(ds);
+                vote[1] = ds.Tables[0].Rows.Count;
+
+                da = new SqlDataAdapter("Select * from Vote where EID = '" + comboBox5.Text + "' And EC3 ='1' ", con);
+                ds = new DataSet();
+                da.Fill(ds);
+                vote[2] = ds.Tables[0].Rows.Count;
+
+                da = new SqlDataAdapter("Select * from Vote where EID = '" + comboBox5.Text + "' And EC4 ='1' ", con);
+                ds = new DataSet();
+                da.Fill(ds);
+                vote[3] = ds.Tables[0].Rows.Count;
+            }
+
+            int maxValue = vote.Max();
+            int maxIndex = vote.ToList().IndexOf(maxValue);
+
+            textBox9.Text = names[maxIndex];
+            textBox8.Text = ""+maxValue;
+
+            cmd = new SqlCommand("Update Election set Result = '"+textBox9.Text+"-"+textBox8.Text+"' where EId = '"+comboBox5.Text+"'",con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+    }
+}
