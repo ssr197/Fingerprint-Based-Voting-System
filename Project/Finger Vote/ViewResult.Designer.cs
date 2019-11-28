032055c8b86f1eaaac77968c69a38e0f9e849e00
Saurabh Kumar <saurabh.kumar1@rxlogix.com>
2019-11-28 17:29:33 +0530

    Added View Result

---
+++ Project/Finger Vote/ViewResult.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Finger_ATM
+{
+    partial class ViewResult
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Navy;
+            this.label1.Location = new System.Drawing.Point(35, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 37);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "View Result";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Finger_ATM.Properties.Resources.Line;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 75);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1275, 18);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(60, 139);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(1250, 420);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(635, 614);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 41);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Export";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // ViewResult
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1370, 750);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "ViewResult";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ViewResult";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.ViewResult_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Project/Finger Vote/ViewResult.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Finger_ATM
+{
+    public partial class ViewResult : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=ADMIN-PC;Initial Catalog=FingerVote;Integrated Security=True"); 
+        
+        public ViewResult()
+        {
+            InitializeComponent();
+        }
+
+        private void ViewResult_Load(object sender, EventArgs e)
+        {
+            SqlDataAdapter da = new SqlDataAdapter("Select * from election where Result <> 'N/A' ",con);
+            DataTable ds = new DataTable();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds;
+
+            DataGridViewColumn c1 = dataGridView1.Columns[0];
+            c1.HeaderText = "Election ID";
+            c1.Width = 110;
+            DataGridViewColumn c2 = dataGridView1.Columns[1];
+            c2.Width = 325;
+            c2.HeaderText = "Election Topic";
+            DataGridViewColumn c3 = dataGridView1.Columns[2];
+            c3.HeaderText = "No Of Candidate";
+            c3.Width = 100;
+            c3.Visible = false;
+            DataGridViewColumn c4 = dataGridView1.Columns[3];
+            c4.HeaderText = "Candidate 1";
+            c4.Width = 130;
+            DataGridViewColumn c5 = dataGridView1.Columns[4];
+            c5.HeaderText = "Candidate 2";
+            c5.Width = 130;
+            DataGridViewColumn c6 = dataGridView1.Columns[5];
+            c6.Width = 130;
+            c6.HeaderText = "Candidate 3";
+            DataGridViewColumn c7 = dataGridView1.Columns[6];
+            c7.Width = 130;
+            c7.HeaderText = "Candidate 4";
+            DataGridViewColumn c8 = dataGridView1.Columns[7];
+            c8.Width = 120;
+            c8.HeaderText = "End Date";
+            DataGridViewColumn c9 = dataGridView1.Columns[8];
+            c9.Width = 130;
+            c9.HeaderText = "Winner";
+        }
+    }
+}
